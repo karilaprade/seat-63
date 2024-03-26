0c736ab8bab77a5176fc8b9008a88dacd940b6f6
karilaprade <klaprade@live.com>
2024-03-26 14:57:08 -0700

    Initial commit

---
+++ scripts/delayed.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import { sampleRUM, loadScript } from './aem.js';
+
+// Core Web Vitals RUM collection
+sampleRUM('cwv');
+
+// add more delayed functionality here
